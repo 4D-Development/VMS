709bb3977f36054da1a8df16e2b9b0d387e045ab
Davidmunera07 <n00919778@ospreys.unf.edu>
2018-08-19 15:58:27 -0400

    whatever

---
+++ VMS/Models/Volunteer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VMS.Models
+{
+    public class Volunteer
+    {
+        public int VolunteerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string CentersPreferredToWork { get; set; }
+        public string SkillsInterests { get; set; }
+        public string AvailabilityTimes { get; set; }
+        public string Address { get; set; }
+        public string HomeWorkCellPhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string EducationalBackground { get; set; }
+        public string CurrentLicenses { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactHomeWorkPhone { get; set; }
+        public string EmergencyContactEmail { get; set; }
+        public string EmergencyContactAddress { get; set; }
+        public bool CopyOfVolunteerDriverLicenseOnFile { get; set; }
+        public bool CopyOfVolunteerSocialSecurityCardOnFile { get; set; }
+        public string ApprovalStatus { get; set; }
+    }
+}
--- VMS/Startup.cs
+++ VMS/Startup.cs
@@ -6,28 +6,44 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using VMS.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace VMS
 {
     public class Startup
     {
+        IConfigurationRoot Configuration;
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json").Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IVolunteerRepository, EFVolunteerRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory
+loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Volunteer}/{action=List}/{id?}");
             });
         }
     }
